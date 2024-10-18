c3a6e95fecdc29a68772e93299164330e70b0007
lincgr <lincgr@gmail.com>
2024-10-18 17:57:52 +0200

    02_05 Análisis de Sentimientos usando NLTK

---
+++ 02_05/02_05.py
@@ -0,0 +1,8 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+nltk.download('vader_lexicon')
+
+texto = "I love this app, it is very easy to use and fast. However, it sometimes freezes."
