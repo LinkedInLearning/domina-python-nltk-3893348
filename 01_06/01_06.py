f1344caff8c1408d769f19ec817c85e70f256192
lincgr <lincgr@gmail.com>
2024-10-18 17:47:33 +0200

    01_06 Construcción de Vocabulario usando NLTK

---
+++ 01_06/01_06.py
@@ -0,0 +1,6 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+
+texto = "El procesamiento de lenguaje natural es fascinante. La inteligencia artificial y el aprendizaje automático son campos muy prometedores."
