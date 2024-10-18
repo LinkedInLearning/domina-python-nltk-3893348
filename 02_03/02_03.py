69bb83e2089bef08fa522ad50ec4cc2fd8a3ab36
lincgr <lincgr@gmail.com>
2024-10-18 17:55:38 +0200

    02_03 Frecuencia de Palabras usando NLTK

---
+++ 02_03/02_03.py
@@ -0,0 +1,8 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
+
+nltk.download('punkt')
+
+texto = "La inteligencia artificial está revolucionando el mundo. La inteligencia artificial está presente en muchas áreas de nuestras vidas."
+
