a6fb5ded03dff1ec85f627b860f90d88d0ca4a13
lincgr <lincgr@gmail.com>
2024-10-18 17:57:18 +0200

    02_04 Extracción de Características (TF-IDF) usando NLTK

---
+++ 02_04/02_04.py
@@ -0,0 +1,18 @@
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+nltk.download('punkt')
+
+documentos = [
+    "La nueva aplicación es rápida e intuitiva.",
+    "Esta aplicación es muy fácil de usar.",
+    "Me encanta la interfaz rápida de la aplicación."
+]
+
+vectorizador = TfidfVectorizer()
+X = vectorizador.fit_transform(documentos)
+
+print(vectorizador.get_feature_names_out())
+print(X.toarray())
